6397bd448e03b0ad570be94000003d7437a78b80
Vansh Singh <vanshchaudhary@gmail.com>
2020-01-31 15:25:04 -0800

    benchmark fork

--- benchmark.h
+++ benchmark.h
@@ -8,6 +8,8 @@
 
 #include "constants.h"
 #include "cpu_tests.h"
+#include <sys/types.h>
+#include <unistd.h>
 
 extern uint64_t benchmarkCycles(fun_ptr test) {
   uint32_t cycles_high0, cycles_low0, cycles_low1, cycles_high1;
@@ -36,3 +38,34 @@ extern uint64_t benchmarkCycles(fun_ptr test) {
   return end-start;
 }
 
+extern uint64_t benchmarkFork() {
+  uint32_t cycles_high0, cycles_low0, cycles_low1, cycles_high1;
+  
+  pid_t parentId = getpid();
+
+  asm volatile (
+    "CPUID\n\t"
+		"RDTSC\n\t"
+		"mov %%edx, %0\n\t"
+		"mov %%eax, %1\n\t": "=r" (cycles_high0), 
+    "=r" (cycles_low0)::"rax", "%rbx", "%rcx", "%rdx"
+  );
+  
+  fork();
+
+	asm volatile (
+		"RDTSCP\n\t"
+		"mov %%edx, %0\n\t"
+		"mov %%eax, %1\n\t": "=r" (cycles_high1), 
+    "=r" (cycles_low1)::"rax", "%rbx", "%rcx", "%rdx"
+  );
+
+  uint64_t start = ((uint64_t)cycles_high0 << 32) | cycles_low0;
+  uint64_t end = ((uint64_t)cycles_high1 << 32) | cycles_low1;
+
+  if (getpid() != parentId) {
+    exit(0);
+  }
+
+  return end-start;
+}
