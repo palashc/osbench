c1931086116da604bc1d236d63d0a5439b879302
Vansh Singh <vanshchaudhary@gmail.com>
2020-03-07 14:23:48 -0800

    Refactor page_fault test

--- page_fault.h
+++ page_fault.h
@@ -13,9 +13,9 @@ size_t get_file_size(const char *filename) {
 	return st.st_size;
 }
 
-uint64_t benchmark_pf(fun_ptr _ignore)
+uint64_t run_pf(uint32_t iterations, uint32_t trials)
 {
-	const char* FILENAME = "tmp";
+	const char* FILENAME = "fs/7g.o";
 
 	size_t file_size = get_file_size(FILENAME);
 	int fd = open(FILENAME, O_RDONLY);
@@ -24,33 +24,18 @@ uint64_t benchmark_pf(fun_ptr _ignore)
 	char* addr = mmap(NULL, file_size, PROT_READ|PROT_WRITE, MAP_FILE|MAP_PRIVATE, fd, 0);
 	
 	srand(time(NULL));
-	int n = rand() % file_size;
+	long n;
 	
-	uint32_t cycles_high0, cycles_low0, cycles_low1, cycles_high1;
+	void setup() {
+		// rand() only outputs integers up until 65311. Multiply by large prime
+		n = (rand() * 999331)  % file_size;
+	}; 
+
+	void test() {
+		char nth_byte = addr[n];
+	};
 	
-	asm volatile (
-	"CPUID\n\t"
-		"RDTSC\n\t"
-		"mov %%edx, %0\n\t"
-		"mov %%eax, %1\n\t": "=r" (cycles_high0), 
-	"=r" (cycles_low0)::"rax", "%rbx", "%rcx", "%rdx"
-	);
-
-	char nth_byte = addr[n];
-
-	asm volatile (
-		"RDTSCP\n\t"
-		"mov %%edx, %0\n\t"
-		"mov %%eax, %1\n\t": "=r" (cycles_high1), 
-	"=r" (cycles_low1)::"rax", "%rbx", "%rcx", "%rdx"
-	);
-
-	uint64_t start = ((uint64_t)cycles_high0 << 32) | cycles_low0;
-	uint64_t end = ((uint64_t)cycles_high1 << 32) | cycles_low1;
-	uint64_t total = end - start;
-
+	runTestSetup(benchmarkCycles, test, "page fault time", iterations, trials, setup);
 	int unmap_result = munmap(addr, file_size);
 	close(fd);
-
-	return total;
 }
